2d47404b43d99cf9d746afaac37168852cf9b545
steve mutuvi <stevemutuvi@gmail.com>
2020-03-16 12:23:22 +0300

    ciat_ner and api with documentation

--- api/app.py
+++ api/app.py
@@ -1,13 +1,15 @@
 import os
-import pprint
+from os import path
+import io
+import zipfile
 from pathlib import Path
-from flask import Flask, request, jsonify
-from fastai.basic_train import load_learner
 from fastai.vision import *
-from flask_cors import CORS, cross_origin
+from fastai.basic_train import load_learner
+from fastapi import FastAPI, File, UploadFile
+from pathlib import Path
+import shutil
 
-app = Flask(__name__)
-CORS(app, support_credentials=True)
+app = FastAPI()
 
 
 def get_raw_content(raw_directory):
@@ -65,39 +67,20 @@ def predict_many():
         'confidence': probs
     }
 
-
-def predict_many_pretty():
+def predict_many_zipped(zip_file):
     'function takes a test dataset and returns prediction'
-    # # load the learner
-    learn = load_learner(path='./models', file='ciat_image_model.pkl')
-    classes = learn.data.classes
+    
+    Path("./test_dir").mkdir(parents=True, exist_ok=True)
+    # return test_dir
+    # # # load the learner
 
-    test_data_dir = 'test_images/'
-    data_files = get_raw_content(test_data_dir)
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall('./test_dir')
 
     results = []
-    for img_file in data_files:
-        prediction = learn.predict(open_image(
-            os.path.join(Path(test_data_dir), img_file)))
-        probs_list = prediction[2].numpy()
 
-        file_name = img_file
-        category = classes[prediction[1].item()]
-        probs = {c: round(float(probs_list[i]), 5)
-                 for (i, c) in enumerate(classes)}
-        d = {'file_name': file_name, 'prediction': category, 'confidence': probs}
-        results.append(d)
-    return {
-        'results': results
-    }
-
-
-def predict_many_two():
-    'function takes a test dataset and returns prediction'
-    # # load the learner
-    results = []
     learn = load_learner(path='./models', file='ciat_image_model.pkl',
-                         test=ImageList.from_folder('test_images'))
+                         test=ImageList.from_folder('./test_dir'))
     preds, _ = learn.get_preds(ds_type=DatasetType.Test)
     thresh = 0.2
     classes = learn.data.classes
@@ -111,76 +94,34 @@ def predict_many_two():
 
     d = {'file_name': file_name, 'prediction': category, 'confidence': confidences}
     results.append(d)
+    
+    path_dir = pathlib.Path('./test_dir', ignore_errors=True)
+    shutil.rmtree(path_dir)
 
     return {
         'results': results
     }
 
-
-def predict_many_two_pretty():
-    'function takes a test dataset and returns prediction'
-    # # load the learner
-    results = []
-    learn = load_learner(path='./models', file='ciat_image_model.pkl',
-                         test=ImageList.from_folder('test_images'))
-    preds, _ = learn.get_preds(ds_type=DatasetType.Test)
-
-    thresh = 0.2
-    classes = learn.data.classes
-    category = [' '.join([classes[i]
-                          for i, p in enumerate(pred) if p > thresh]) for pred in preds]
-    file_name = [f.name for f in learn.data.test_ds.items]
-
-    preds = preds.tolist()
-
-    confidences = [{c: round((p), 3) for (c, p) in zip(
-        classes, probs)} for probs in preds]
-    mapped = zip(file_name, category, confidences)
-
-    return list(mapped)
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-# route for single prediction
-@app.route('/predict', methods=['POST'])
-def predict():
-    return jsonify(predict_single(request.files['image']))
+@app.post('/predict_single_image')
+async def predict(file: UploadFile = File(...)):
+    # return {"filename": file.filename}
+    return predict_single(file.file)
 
-# route for batch prediction
-@app.route('/predict_batch', methods=['POST', 'GET'])
-def predict_batch():
+@app.post('/predict_batch_zipped')
+async def predict_batch_zipped(file: UploadFile = File(...)):
     '''
     predict batch
     '''
-    return jsonify(predict_many())
+    return predict_many_zipped(file.file)
 
-# route for batch prediction
-@app.route('/predict_batch_pretty', methods=['POST', 'GET'])
-def predict_batch_pretty():
-    '''
-    predict batch pretty
-    '''
-    return jsonify(predict_many_pretty())
+# @app.get('/predict_batch')
+# def predict_batch():
+#     '''
+#     predict batch
+#     '''
+#     return predict_many()
 
-# route for batch prediction
-@app.route('/predict_batch_two', methods=['POST', 'GET'])
-def predict_batch_two():
-    '''
-    predict batch example two
-    '''
-    return jsonify(predict_many_two())
-
-# route for batch prediction - prettified
-@app.route('/predict_batch_two_pretty', methods=['POST', 'GET'])
-def predict_batch_two_pretty():
-    '''
-    predict batch example two pretty
-    '''
-    return jsonify(prettify(predict_many_two_pretty()))
-
-
-if __name__ == '__main__':
-    app.run(debug=False, port=80, host='0.0.0.0')
--- api/start.sh
+++ api/start.sh
@@ -1,2 +1,4 @@
-docker run --name c2h -d -p 80:80 ciat2http
+# docker run --name c2h -d -p 80:80 ciat2http
 # docker run -p 80:80 ciat2http
+
+uvicorn app:app --reload
